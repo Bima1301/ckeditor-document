242e42ce4771c2b3a62e158052a6a39b3a25e342
Yanuar Bima <bima.aji1380@gmail.com>
2024-02-02 14:37:11 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CkEditor from './components/CkEditors'
+
+function App() {
+
+  return (
+    <>
+      <CkEditor />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CkEditors.tsx
@@ -0,0 +1,40 @@
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+//@ts-ignore
+import Editor from "ckeditor5-custom-build";
+
+const editorConfiguration = {
+  toolbar: [
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'outdent',
+    'indent',
+    '|',
+    'imageUpload',
+    'blockQuote',
+    'insertTable',
+    'mediaEmbed',
+    'undo',
+    'redo',
+    'pageBreak',
+  ]
+};
+export default function CkEditors(props: any) {
+  return (
+    <div>
+      <CKEditor
+        editor={Editor}
+        config={editorConfiguration}
+        data={props.initialData}
+        onChange={(event, editor: any) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+        }}
+      />
+    </div>
+  )
+}
